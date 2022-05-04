66e1f91e72a18ca4e41800e5f7aa9dcfc2cfc7e6
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2022-05-04 12:02:06 +0200

    basic grid option added

---
+++ src/js/ui.js
@@ -0,0 +1,54 @@
+const bnLogo = document.querySelector("button.menu-logo")
+const bnExpand = document.querySelector("button.menu-expand")
+const container = document.querySelector("div.container")
+const bnMenu = document.querySelector("button.menu-menu")
+const bnDot = document.querySelectorAll("button.menu-dot")
+const bnClip = document.querySelector("button.menu-clipboard")
+const bn4square = document.querySelector("button.menu-4square")
+const bnAdjust = document.querySelector("button.menu-adjust")
+const bnPulse = document.querySelector("button.menu-pulse")
+const bnCircles = document.querySelectorAll("button.menu-circle")
+const bnSettings = document.querySelector("button.menu-settings")
+
+bnLogo.addEventListener("click", function () {
+  console.log("bnLogo clicked")
+})
+
+bnExpand.addEventListener("click", function () {
+  console.log("bnExpand clicked")
+	container.classList.toggle("expanded")
+	container.classList.toggle("collapsed")
+})
+
+bnMenu.addEventListener("click", function () {
+  console.log("bnMenu clicked")
+})
+
+
+bnClip.addEventListener("click", function () {
+  console.log("bnClip clicked")
+})
+
+bn4square.addEventListener("click", function () {
+  console.log("bn4square clicked")
+})
+bnAdjust.addEventListener("click", function () {
+  console.log("bnAdjust clicked")
+})
+bnPulse.addEventListener("click", function () {
+  console.log("bnPulse clicked")
+})
+bnCircles.forEach(function (bnCircle) {
+  bnCircle.addEventListener("click", function () {
+    console.log("bnCircle clicked")
+  })
+})
+bnSettings.addEventListener("click", function () {
+  console.log("bnSetting clicked")
+})
+
+bnDot.forEach(function (bnDot) {
+  bnDot.addEventListener("click", function () {
+	console.log("bnDot clicked")
+  })
+})
