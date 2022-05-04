0875a56c136b1b298f362da8f055cfc7104a69bc
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2022-05-04 12:17:52 +0200

    Update main.css
    style adjustments to logos

--- src/js/ui.js
+++ src/js/ui.js
@@ -9,6 +9,11 @@ const bnAdjust = document.querySelector("button.menu-adjust")
 const bnPulse = document.querySelector("button.menu-pulse")
 const bnCircles = document.querySelectorAll("button.menu-circle")
 const bnSettings = document.querySelector("button.menu-settings")
+const bnUser = document.querySelector("button.header-user")
+const bnItemExpand = document.querySelectorAll("div.content-item-title button")
+const contentItems = document.querySelectorAll("div.content-item")
+const contentItemInputs = document.querySelectorAll("div.content-item input")
+
 
 bnLogo.addEventListener("click", function () {
   console.log("bnLogo clicked")
@@ -51,4 +56,28 @@ bnDot.forEach(function (bnDot) {
   bnDot.addEventListener("click", function () {
 	console.log("bnDot clicked")
   })
+})
+
+bnUser.addEventListener("click", function () {
+  console.log("bnUser clicked")
+})
+
+bnItemExpand.forEach(function (bnItem) {
+	bnItem.addEventListener("click", function (event) {
+		console.log("bnItemExpand clicked")
+		const currentItem = event.target;
+		contentItems.forEach(function (contentItem) {
+			if (contentItem.contains(currentItem)) {
+				contentItem.classList.toggle("expanded")
+			}else{
+				contentItem.classList.toggle("hidden")
+			}
+		})
+	})
+})
+
+contentItemInputs.forEach(function (inputItem) {
+	inputItem.addEventListener("input", function (event) {
+		console.log(event.target.id, event.target.value, event.target.getAttribute("data-id"))
+	})
 })
